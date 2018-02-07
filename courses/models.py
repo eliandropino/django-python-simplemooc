0f184387c3b8b7278ee90adf80d339fc2f691457
Eliandro Selmar Bikoski <eliandrobikoski@ftec.com.br>
2018-02-07 11:41:34 -0200

    adicionado cursos, imagens, detalhes do curso

--- courses/models.py
+++ courses/models.py
@@ -1,13 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) |
+            models.Q(description__icontains=query)
+        )
+
 
 class Course(models.Model):
+#para alterações  em modelos no banco de dados
+#1 - fazer as alterações no modelo
+#2 - ativar o amb env e entrar no projeto
+#3 - executar python manage.py makemigrations
+#4 - python manage.py migrate
 
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
-    description = models.TextField('Descrição', blank=True)
+    description = models.TextField('Descrição Simples', blank=True)
+    about = models.TextField('Sobre o Curso', blank=True)
     start_date = models.DateField('Data de Início', null=True, blank=True)
-    image = models.ImageField (upload_to='courses/images', verbose_name='Imagem')
+    image = models.ImageField (upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
     created_at = models.DateTimeField('Criado em ', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
+
+
+def courses(request):
+    template_name = 'courses/courses.html'
+    cursos = Course.objects.all()
+    context = {
+        'cursos':cursos
+    }
+    return render(request, template_name, context)
+
+def details(request, pk):
+    curso = Course.objects.get(pk=pk)
+    context = {
+        'curso':curso
+    }
+    template_name = 'courses/detail.html'
+    return render(request, template_name, context)
